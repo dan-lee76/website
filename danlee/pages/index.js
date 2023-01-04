227da6d750e54d72680bfcb48f4ce935aa35364f
Dan Lee <dan-lee1@hotmail.co.uk>
2023-01-04 16:55:16 +0000

    next.js/tailwind init

---
+++ danlee/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+import {useRouter} from "next/router";
+import ContainerBlock from "./container";
+import Hero from "../components/Hero";
+import NavBar from "../components/NavBar";
+
+export default function Home({ repositories }) {
+    return (
+        <ContainerBlock
+            title="Dan Lee"
+            description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+        >
+            <NavBar></NavBar>
+        <Hero></Hero>
+        </ContainerBlock>
+    );
+}
